41b67348af2f86c7de7a658fdc68ee089cadfc8d
Chris Claude <christ.tchambila@gmail.com>
2025-05-16 13:48:11 +0200

    Update construct binary tree algo

--- AlgorGround/LeetCode/106.ConstructBinaryTree/ConstructBinaryTree.cpp
+++ AlgorGround/LeetCode/106.ConstructBinaryTree/ConstructBinaryTree.cpp
@@ -6,79 +6,68 @@
 //
 
 #include "ConstructBinaryTree.hpp"
-#include "queue"
+#include <queue>
 #include <iostream>
 
-int preorderIndex;
-std::unordered_map<int, int> inorder_map;
+ConstructBinaryTree::TreeNode* ConstructBinaryTree::buildTreeHelper(std::vector<int>& preorder, int left, int right) {
+    if (left > right) return nullptr;
 
-
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-};
-
-TreeNode* buildTreeHelper(std::vector<int>& preorder, int left, int right) {
-    if (left < right) return nullptr;
-    
     TreeNode* root = new TreeNode(preorder[preorderIndex++]);
-    
-    root->left = buildTreeHelper(preorder, left, inorder_map[preorderIndex] + 1);
-    
-    root->right = buildTreeHelper(preorder, inorder_map[preorderIndex] - 1, right);
-    
+
+    int rootIndex = inorderMap[root->val];
+
+    root->left = buildTreeHelper(preorder, left, rootIndex - 1);
+    root->right = buildTreeHelper(preorder, rootIndex + 1, right);
+
     return root;
-};
+}
 
-TreeNode* buildTree(std::vector<int>& preorder, std::vector<int>& inorder)  {
+ConstructBinaryTree::TreeNode* ConstructBinaryTree::buildTree(std::vector<int>& preorder, std::vector<int>& inorder) {
     preorderIndex = 0;
-    
-    for (int i = 0; i < inorder.size(); i++) {
-        inorder_map[inorder[i]] = i;
-    }
-    
-    return buildTreeHelper(preorder, 0, int(inorder.size()) - 1);
-};
+    inorderMap.clear();
 
-bool traverseTree(TreeNode* root, std::vector<int>& preoder) {
+    for (int i = 0; i < inorder.size(); i++) {
+        inorderMap[inorder[i]] = i;
+    }
+
+    return buildTreeHelper(preorder, 0, int(inorder.size()) - 1);
+}
+
+bool ConstructBinaryTree::traverseTree(TreeNode* root, std::vector<int>& preorder) {
     std::queue<TreeNode*> queue;
     int index = 0;
     queue.push(root);
-    
+
     while (!queue.empty()) {
         TreeNode* node = queue.front();
         queue.pop();
-        
-        if (node->val != preoder[index++]) {
+
+        if (node->val != preorder[index++]) {
             return false;
         }
-        
+
         if (node->left != nullptr) {
             queue.push(node->left);
         }
-        
+
         if (node->right != nullptr) {
             queue.push(node->right);
         }
     }
-    
+
     return true;
 }
 
-void runTests() {
-    std::vector<int> preoder1 = {3,9,20,15,7}; // BFS
-    std::vector<int> inorder1 = {9,3,15,20,7}; // DFS
-    auto tree1 = buildTree(preoder1, inorder1);
-    
-    if (traverseTree(tree1, preoder1)) {
+void ConstructBinaryTree::runTests() {
+    std::vector<int> preorder1 = {3,9,20,15,7};
+    std::vector<int> inorder1 = {9,3,15,20,7};
+    auto tree1 = buildTree(preorder1, inorder1);
+
+    if (traverseTree(tree1, preorder1)) {
         std::cout << "Test 1 passed" << "\n";
     } else {
         std::cout << "Test 1 failed" << "\n";
     }
-};
+}
 
 
--- AlgorGround/LeetCode/106.ConstructBinaryTree/ConstructBinaryTree.hpp
+++ AlgorGround/LeetCode/106.ConstructBinaryTree/ConstructBinaryTree.hpp
@@ -14,12 +14,23 @@
 
 class ConstructBinaryTree {
 public:
-   
-    struct TreeNode {};
-    
+    struct TreeNode {
+        int val;
+        TreeNode *left;
+        TreeNode *right;
+        TreeNode() : val(0), left(nullptr), right(nullptr) {}
+        TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+        TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+    };
+
     TreeNode* buildTree(std::vector<int>& preorder, std::vector<int>& inorder);
-    
-    void runTests ();
+    void runTests();
+
+private:
+    int preorderIndex;
+    std::unordered_map<int, int> inorderMap;
+    TreeNode* buildTreeHelper(std::vector<int>& preorder, int left, int right);
+    bool traverseTree(TreeNode* root, std::vector<int>& preorder);
 };
 
 #endif /* ConstructBinaryTree_hpp */
